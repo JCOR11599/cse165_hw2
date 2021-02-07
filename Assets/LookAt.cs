ce0c2464a24d68a093a3f76857aa5f15be57451e
James Cor <jcor@ucsd.edu>
2021-02-06 19:39:59 -0800

    arrow movement works

---
+++ Assets/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(target);
+        transform.eulerAngles = new Vector3(70.0f, transform.eulerAngles.y, transform.eulerAngles.z);   
+    }
+}
