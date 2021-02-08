ad740cf070e449d8ea5f83380288306ebc943d9d
James Cor <jcor@ucsd.edu>
2021-02-07 16:46:17 -0800

    finished with EC 2

--- Assets/MouseMovement.cs
+++ Assets/MouseMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class MouseMovement : MonoBehaviour
 {
     // Set mouse sensitivity
-    public float lookSensitivity = 0.5f;
-    public float movementSpeed = 0.05f;
+    private float lookSensitivity = 0.5f;
+    private float movementSpeed = 0.05f;
+    private float flyingMouseSensitivity = 50.0f;
+    private float flyingMoveSpeed = 50.0f;
 
     // Track mouse positions
     private Vector2 prevPos;
@@ -15,47 +17,90 @@ public class MouseMovement : MonoBehaviour
 
     // Track yaw (left and right rotations)
     private float yaw;
+    private float pitch;
     private Vector3 transformPosition;
 
+    // GameManager to indicate when game is over
     private GameManager gameManager;
+
+    // Flying
+    private bool flying;
 
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         yaw = transform.eulerAngles.y;
         transformPosition = transform.position;
+        flying = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // when left mouse button is pressed
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown("f"))
         {
-            prevPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            flying = !flying;
         }
 
-        // when left mouse button is held
-        if (Input.GetMouseButton(0))
+        if (flying)
         {
-            // get current mouse position
-            currPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            // Disable gravity
+            this.gameObject.GetComponent<Rigidbody>().useGravity = false;
 
-            // calculate amount of change
-            amount = currPos - prevPos;
+            // Mouse movement
+            yaw += Input.GetAxis("Mouse X") * flyingMouseSensitivity * Time.deltaTime;
+            pitch -= Input.GetAxis("Mouse Y") * flyingMouseSensitivity * Time.deltaTime;
 
-            // calculate left and right rotations
-            yaw += amount.x * lookSensitivity * Time.deltaTime;
-            yaw %= 360;
+            // Constrain yaw and pitch
+            yaw %= 360.0f;
+            pitch = Mathf.Clamp(pitch, -90.0f, 90.0f);
 
-            // move camera
-            transform.position += transform.forward * amount.y * movementSpeed * Time.deltaTime;
-            transformPosition = transform.position;
+            // Move camera
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+            // Keyboard controls
+            if (Input.GetKey("w"))
+                transform.position += transform.forward * flyingMoveSpeed * Time.deltaTime;
+            if (Input.GetKey("s"))
+                transform.position -= transform.forward * flyingMoveSpeed * Time.deltaTime;
+            if (Input.GetKey("a"))
+                transform.position -= transform.right * flyingMoveSpeed * Time.deltaTime;
+            if (Input.GetKey("c"))
+                transform.position += transform.right * flyingMoveSpeed * Time.deltaTime;
         }
+        else
+        {
+            // Enable gravity
+            this.gameObject.GetComponent<Rigidbody>().useGravity = true;
 
-        // rotate camera
-        transform.position = transformPosition;
-        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+            // when left mouse button is pressed
+            if (Input.GetMouseButtonDown(0))
+            {
+                prevPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+
+            // when left mouse button is held
+            if (Input.GetMouseButton(0))
+            {
+                // get current mouse position
+                currPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+                // calculate amount of change
+                amount = currPos - prevPos;
+
+                // calculate left and right rotations
+                yaw += amount.x * lookSensitivity * Time.deltaTime;
+                yaw %= 360;
+
+                // move camera
+                transform.position += transform.forward * amount.y * movementSpeed * Time.deltaTime;
+                transformPosition = transform.position;
+            }
+
+            // rotate camera
+            transform.position = transformPosition;
+            transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+        }
     }
 
     void OnTriggerEnter(Collider other)
